9adaceee36ce2f97fdee563190e91c20a1ba06ca
Gabriel Ellnebrand <gabriel.ellnebrand@gmail.com>
2025-07-17 16:28:33 +0200

    Initial commit for 1.21.5 version of mineward-utils

---
+++ src/client/java/mineward/utils/MinewardUtilsClient.java
@@ -0,0 +1,10 @@
+package mineward.utils;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MinewardUtilsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
